056e3140dd58a37084dff11f4e4de295ac0c0ec8
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-16 22:37:55 +0200

    first commit

---
+++ savings.py
@@ -0,0 +1,13 @@
+savings = 0
+years = 0
+
+monthly_saving = int(input("How many dollars can you save per month? "))
+
+while(years < 40):
+    savings += monthly_saving * 12
+    savings *= 1.12  # increases with average of 12 % every year
+
+    years += 1
+
+    if (years < 10 or years % 10 == 0):
+        print("$", '{:,}'.format(round(savings)), "after", years, "years")
